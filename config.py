8f591f759e8f8bcb7a499239742888d209f5e754
allentertainmentsstudio <allentertainmentsstudio@gmail.com>
2026-03-05 14:07:03 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+FORCE_CHANNEL = os.getenv("FORCE_CHANNEL")
+
+DOWNLOAD_DIR = "downloads"
