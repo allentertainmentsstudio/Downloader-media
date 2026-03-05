3065ea78d27a47d8aacbc4ad7d5309e10c793b25
allentertainmentsstudio <allentertainmentsstudio@gmail.com>
2026-03-05 16:48:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
 
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+API_ID = int(os.getenv("34724970"))
+API_HASH = os.getenv("f240eae7c60e8e30c17203ab0e052f7e")
+BOT_TOKEN = os.getenv("8799486461:AAH6td7_S68a3PnGNJermUgxA6_A1vhBS-c")
 
-FORCE_CHANNEL = os.getenv("FORCE_CHANNEL")
+FORCE_CHANNEL = os.getenv("@Anujedits76")
 
 DOWNLOAD_DIR = "downloads"
